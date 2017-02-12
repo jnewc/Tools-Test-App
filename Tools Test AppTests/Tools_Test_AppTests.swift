76ff0072f8dfeccd3c627279a85dddaaf880870f
Jack Newcombe <jack.newcombe@just-eat.com>
2017-02-12 00:40:55 +0000

    Add sample unit tests (failing, passing, and passing with coverage) and enable coverage for scheme

--- Tools Test AppTests/Tools_Test_AppTests.swift
+++ Tools Test AppTests/Tools_Test_AppTests.swift
@@ -21,16 +21,17 @@ class Tools_Test_AppTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+	func testPassing() {
+		XCTAssert(true)
+	}
+	
+	func testFailing() {
+		XCTFail()
+	}
+	
+	func testViewController() {
+		let viewController = ViewController()
+		XCTAssertNotNil(viewController)
+	}
+		
 }
